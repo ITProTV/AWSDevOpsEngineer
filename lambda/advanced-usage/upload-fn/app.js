444d37b36717c68f3a8a21e6f30c942ab14cbcc3
Justin Dennison <justin1dennison@gmail.com>
2018-12-03 16:10:03 -0500

    upload fn responder function

--- lambda/advanced-usage/upload-fn/app.js
+++ lambda/advanced-usage/upload-fn/app.js
@@ -1,18 +1,33 @@
-let response;
+const AWS = require('aws-sdk')
+AWS.config.update({ region: 'us-east-1' })
 
-exports.handler = async (event, context) => {
-    try {
-        return { 
-	    'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+const sns = new AWS.SNS()
+const dynamo = new AWS.DynamoDB()
+const { TABLE_NAME, TOPIC_ARN } = process.env
 
-    return response
-};
+exports.handler = (event, context) => {
+   const { Records } = event
+   const objects = Records.map(r => ({ 
+     bucket: r.s3.bucket.name, 
+     key: r.s3.object.key 
+   })) 
+   return Promise.all(objects.map(async object => {
+     const item = { TableName: TABLE_NAME, Item: createDBItem(object) }
+     await dynamo.putItem(item).promise()
+     const message = JSON.stringify(item)	
+     await sns.publish({ Message: message, TopicArn: TOPIC_ARN }).promise()
+     return message
+   })).then(messages => context.done(null, messages))
+      .catch(err => context.done(err))
+   
+}
+
+function createDBItem(object) {
+	return {
+	   filename: {S: object.key },
+	   bucket: {S: object.bucket },
+	   completed: {S: 'new' }
+	}
+}
+
+
