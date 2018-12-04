2209e923abb0d287daebd93dcab2e1c9ddef888c
Justin Dennison <justin1dennison@gmail.com>
2018-12-04 09:48:18 -0500

    added the notify function for the lambda usage

--- lambda/advanced-usage/notify-fn/app.js
+++ lambda/advanced-usage/notify-fn/app.js
@@ -1,18 +1,20 @@
-let response;
+const AWS = require('aws-sdk')
+
+AWS.config.update({ region: 'us-east-1' })
+
+const sns = new AWS.SNS()
+const { TOPIC_ARN } = process.env
 
 exports.handler = async (event, context) => {
-    try {
-        return {
-	    'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
-    return response
+  return await Promise.all(event.Records.map(async record => {
+    const Keys = record.dynamodb.Keys
+    const filename = Keys.filename.S
+    const params = {
+      Message: `Filename: ${filename} has been transcribed`,
+      Subject: `${new Date().toString()} Transcription`,
+      TopicArn: TOPIC_ARN
+    } 
+    await sns.publish(params).promise()
+    console.log("#### Message Sent ####") 
+  }))
 }
