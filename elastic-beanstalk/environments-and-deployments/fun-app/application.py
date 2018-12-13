e6d188039c760c9c98b00c4a675d167fab349b3d
Justin Dennison <justin1dennison@gmail.com>
2018-12-13 08:10:22 -0500

    updated for elastic beanstalk and ecs requirements

--- elastic-beanstalk/environments-and-deployments/fun-app/application.py
+++ elastic-beanstalk/environments-and-deployments/fun-app/application.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 alarm = os.getenv("MODE") != "PRODUCTION"
 version = os.getenv("VERSION")
 
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template("index.html", alarm=alarm, version=version)
